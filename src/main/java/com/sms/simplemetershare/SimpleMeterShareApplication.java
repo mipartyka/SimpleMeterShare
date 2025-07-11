8e819a044cc6863c267f6c22b0c8d80872fff2e0
Michał <michal.t.partyka@gmail.com>
2025-07-11 14:58:25 +0200

    Initial commit

---
+++ src/main/java/com/sms/simplemetershare/SimpleMeterShareApplication.java
@@ -0,0 +1,13 @@
+package com.sms.simplemetershare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMeterShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMeterShareApplication.class, args);
+    }
+
+}
